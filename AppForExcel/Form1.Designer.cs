1ad4aa6ab6a1e5ab9ac81ba9e41f1b40b47f54d7
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2022-04-09 04:04:25 +0900

    プロジェクト ファイルを追加します。

---
+++ AppForExcel/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace AppForExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.HeightPixel = new System.Windows.Forms.TextBox();
+            this.WidthPixel = new System.Windows.Forms.TextBox();
+            this.SpriteNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.HeightPixel);
+            this.splitContainer1.Panel2.Controls.Add(this.WidthPixel);
+            this.splitContainer1.Panel2.Controls.Add(this.SpriteNumber);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.splitContainer1.Size = new System.Drawing.Size(518, 327);
+            this.splitContainer1.SplitterDistance = 298;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(514, 293);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.OnImageClick);
+            // 
+            // HeightPixel
+            // 
+            this.HeightPixel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.HeightPixel.Location = new System.Drawing.Point(285, 2);
+            this.HeightPixel.Name = "HeightPixel";
+            this.HeightPixel.Size = new System.Drawing.Size(38, 23);
+            this.HeightPixel.TabIndex = 8;
+            // 
+            // WidthPixel
+            // 
+            this.WidthPixel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.WidthPixel.Location = new System.Drawing.Point(175, 1);
+            this.WidthPixel.Name = "WidthPixel";
+            this.WidthPixel.Size = new System.Drawing.Size(38, 23);
+            this.WidthPixel.TabIndex = 7;
+            // 
+            // SpriteNumber
+            // 
+            this.SpriteNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SpriteNumber.Location = new System.Drawing.Point(438, 0);
+            this.SpriteNumber.Name = "SpriteNumber";
+            this.SpriteNumber.ReadOnly = true;
+            this.SpriteNumber.Size = new System.Drawing.Size(75, 23);
+            this.SpriteNumber.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "チップ番号";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "高さ(px)";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(11, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ファイル選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "幅(px)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 327);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.OnFormSizeChanged);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SpriteNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox HeightPixel;
+        private System.Windows.Forms.TextBox WidthPixel;
+    }
+}
+
---
+++ AppForExcel/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppForExcel
+{
+    public partial class Form1 : Form
+    {
+        private Image image = null;
+        private float scalar = 0.95f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            //インスタンス生成
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = "PNGファイル|*.png|JPGファイル|*.jpg;*.jpeg";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                if (image != null)
+                {
+                    image.Dispose();
+                }
+                string filePath = openFile.FileName;
+                image = Image.FromFile(filePath);
+                pictureBox1.Image = image;
+                pictureBox1.Size = image.Size;
+            }
+        }
+
+        private void OnImageClick(object sender, EventArgs e)
+        {
+            //マウスと画像の原点のモニター上のスクリーン座標を取得
+            System.Drawing.Point mousePoint = MousePosition;
+            System.Drawing.Point pictureOrigin = PointToScreen(pictureBox1.Location);
+
+            //取得した座標からマウスが画像上のどの位置にいるのか求める
+            int x = mousePoint.X - pictureOrigin.X;
+            int y = mousePoint.Y - pictureOrigin.Y;
+
+            int chipWidth;
+            int chipHeight;
+            int spritePerRow;
+
+            //後の計算が正常に実行されない値が入力されていた場合は中断する
+            if(WidthPixel.TextLength == 0
+                || WidthPixel.Text == "0"
+                || !int.TryParse(WidthPixel.Text, out chipWidth)
+                || HeightPixel.TextLength == 0
+                || HeightPixel.Text == "0"
+                || !int.TryParse(HeightPixel.Text, out chipHeight))
+            {
+                SpriteNumber.Text = "NaN";
+                return;
+            }
+
+            //画像の一列の中に何枚のチップがあるか求める
+            spritePerRow = pictureBox1.Width / chipWidth;
+
+            //クリックしたチップの番号を求める
+            int spriteNum = (y / chipHeight) * spritePerRow + x / chipWidth;
+            SpriteNumber.Text = spriteNum.ToString();
+        }
+
+        private void OnFormSizeChanged(object sender, EventArgs e)
+        {
+            //フォームのサイズが変更されたらコンテナのサイズも変更させる
+            //100%ではスクロールバーが画面外に出てしまうため補正する
+            splitContainer1.Width = (int)(ActiveForm.Width * scalar);
+            splitContainer1.Height = (int)(ActiveForm.Height * scalar);
+            splitContainer1.SplitterDistance = (int)(splitContainer1.Height * scalar);
+        }
+    }
+}
